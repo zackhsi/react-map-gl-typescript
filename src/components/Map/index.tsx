c403bbc42a32224b93f8d24305bb0f590ff6ae4a
Zack Hsi <zackhsi@gmail.com>
2018-07-08 13:06:02 -0700

    --wip--

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+export default class Map extends React.Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9'
+    });
+  }
+
+  componentWillUnmount() {
+    this.map.remove();
+  }
+
+  render() {
+    const style {
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      width: '100%'
+    };
+
+    return <div style={style} ref={el => this.mapContainer = el} />;
+  }
+}
+
+ReactDOM.render(<Map />, document.getElementById('app'));  
